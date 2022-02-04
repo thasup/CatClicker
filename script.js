d13d333f6971d1db478f7871a5a7a10006eb22ce
thasup <thanachonfirst@hotmail.com>
2022-02-04 15:50:00 +0700

    Refactor code for scaling number of cats

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const cat = document.getElementsByTagName("div");
-const catName = document.getElementsByTagName("h2");
-const clickTimes = document.getElementsByTagName("h3");
-
 for (let i = 0; i < cat.length; i++) {
+    console.log(cat);
+    cat[i].innerHTML = `<img src="./img/cat${i}.jpg" alt="cat" />`;
+
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    cat[i].appendChild(h2);
+    cat[i].appendChild(h3);
+
     let click = 0;
-    cat[i].addEventListener("click", function (e) {
+    const catName = document.getElementsByTagName("h2");
+    const catImage = document.getElementsByTagName("img");
+    const clickTimes = document.getElementsByTagName("h3");
+
+    catName[i].textContent = `Cat Name : ${cat[i].id}`;
+    clickTimes[i].textContent = `Your click on ${cat[i].id} is ${click} times!`;
+
+    catImage[i].addEventListener("click", function (e) {
         e.preventDefault();
         click++;
-        catName[i].textContent = `Cat Name : ${cat[i].id}`;
-        clickTimes[i].textContent = `Your click is ${click} times!`;
+        clickTimes[
+            i
+        ].textContent = `Your click on ${cat[i].id} is ${click} times!`;
     });
 }
